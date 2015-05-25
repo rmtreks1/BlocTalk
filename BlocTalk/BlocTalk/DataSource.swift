fcc322d07ef852f828305bb78c444e4e52808fe6
Roshan Mahanama <rmtreks@gmail.com>
2015-05-25 12:46:28 +1000

    creating singleton DataSource

---
+++ BlocTalk/BlocTalk/DataSource.swift
@@ -0,0 +1,18 @@
+//
+//  DataSource.swift
+//  BlocTalk
+//
+//  Created by Roshan Mahanama on 25/05/2015.
+//  Copyright (c) 2015 RMTREKS. All rights reserved.
+//
+
+// singleton class
+
+import UIKit
+
+class DataSource: NSObject {
+    
+    static let sharedInstance = DataSource()
+    
+   
+}
