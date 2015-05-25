f565f35d52098e0e05991cea4eb32a6e86d73c1a
Roshan Mahanama <rmtreks@gmail.com>
2015-05-25 10:05:01 +1000

    connecting to other peers

--- BlocTalk/BlocTalk/ViewController.swift
+++ BlocTalk/BlocTalk/ViewController.swift
@@ -7,12 +7,41 @@
 //
 
 import UIKit
+import MultipeerConnectivity
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MCBrowserViewControllerDelegate, MCSessionDelegate {
 
+    
+    let serviceType = "BlockTalk"
+    
+    var browser : MCBrowserViewController!
+    var assistant : MCAdvertiserAssistant!
+    var session : MCSession!
+    var peerID: MCPeerID!
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        self.peerID = MCPeerID(displayName: UIDevice.currentDevice().name)
+        self.session = MCSession(peer: peerID)
+        self.session.delegate = self
+        
+        // create the browser viewcontroller with a unique service name
+        self.browser = MCBrowserViewController(serviceType:serviceType,
+            session:self.session)
+        
+        self.browser.delegate = self;
+        
+        self.assistant = MCAdvertiserAssistant(serviceType:serviceType,
+            discoveryInfo:nil, session:self.session)
+        
+        // tell the assistant to start advertising our fabulous chat
+        self.assistant.start()
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,5 +50,69 @@ class ViewController: UIViewController {
     }
 
 
+    
+    
+    func browserViewControllerDidFinish(
+        browserViewController: MCBrowserViewController!)  {
+            // Called when the browser view controller is dismissed (ie the Done
+            // button was tapped)
+            
+            self.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    func browserViewControllerWasCancelled(
+        browserViewController: MCBrowserViewController!)  {
+            // Called when the browser view controller is cancelled
+            
+            self.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    func session(session: MCSession!, didReceiveData data: NSData!,
+        fromPeer peerID: MCPeerID!)  {
+            // Called when a peer sends an NSData to us
+            
+            // This needs to run on the main queue
+            dispatch_async(dispatch_get_main_queue()) {
+                
+                var msg = NSString(data: data, encoding: NSUTF8StringEncoding)
+                
+            }
+    }
+    
+    // The following methods do nothing, but the MCSessionDelegate protocol
+    // requires that we implement them.
+    func session(session: MCSession!,
+        didStartReceivingResourceWithName resourceName: String!,
+        fromPeer peerID: MCPeerID!, withProgress progress: NSProgress!)  {
+            
+            // Called when a peer starts sending a file to us
+    }
+    
+    func session(session: MCSession!,
+        didFinishReceivingResourceWithName resourceName: String!,
+        fromPeer peerID: MCPeerID!,
+        atURL localURL: NSURL!, withError error: NSError!)  {
+            // Called when a file has finished transferring from another peer
+    }
+    
+    func session(session: MCSession!, didReceiveStream stream: NSInputStream!,
+        withName streamName: String!, fromPeer peerID: MCPeerID!)  {
+            // Called when a peer establishes a stream with us
+    }
+    
+    func session(session: MCSession!, peer peerID: MCPeerID!,
+        didChangeState state: MCSessionState)  {
+            // Called when a connected peer changes state (for example, goes offline)
+            
+    }
+    
+    
+    
+    @IBAction func showBrowser(sender: UIButton) {
+        // Show the browser view controller
+        self.presentViewController(self.browser, animated: true, completion: nil)
+    }
+    
+    
 }
 
