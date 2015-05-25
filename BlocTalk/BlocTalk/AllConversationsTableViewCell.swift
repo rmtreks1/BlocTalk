8fc6bf5ac911f2591c3af9d3adce843b736849f4
Roshan Mahanama <rmtreks@gmail.com>
2015-05-25 18:13:56 +1000

    laying out AllConversationsCell

---
+++ BlocTalk/BlocTalk/AllConversationsTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  AllConversationsTableViewCell.swift
+//  BlocTalk
+//
+//  Created by Roshan Mahanama on 25/05/2015.
+//  Copyright (c) 2015 RMTREKS. All rights reserved.
+//
+
+import UIKit
+
+class AllConversationsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
