add4e884dfa31768f37e8af6211105ddeb266d1a
Roshan Mahanama <rmtreks@gmail.com>
2015-05-26 20:10:18 +1000

    creating Peers view controller to browse peers

--- BlocTalk/BlocTalk/PeersBrowserTableViewController.swift
+++ BlocTalk/BlocTalk/PeersBrowserTableViewController.swift
@@ -93,5 +93,11 @@ class PeersBrowserTableViewController: UITableViewController {
         // Pass the selected object to the new view controller.
     }
     */
+    
+    @IBAction func cancelButtonPressed(sender: UIBarButtonItem) {
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    
 
 }
