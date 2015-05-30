b8997bfe8330d746906d03dc1e8d7d294ad18fa3
Roshan Mahanama <rmtreks@gmail.com>
2015-05-31 08:49:09 +1000

    attempt to deDupe peers list

--- BlocTalk/BlocTalk/PeersBrowserTableViewController.swift
+++ BlocTalk/BlocTalk/PeersBrowserTableViewController.swift
@@ -177,4 +177,33 @@ class PeersBrowserTableViewController: UITableViewController, MPCManagerDelegate
         self.dismissViewControllerAnimated(true, completion: nil)
     }
 
+    
+    
+    
+    @IBAction func lookForDuplicates(sender: UIBarButtonItem) {
+        
+        let uniqueAvailablePeers = uniq(DataSource.sharedInstance.availablePeers)
+        println("count of unique peers: \(uniqueAvailablePeers.count) out of total available peers: \(DataSource.sharedInstance.availablePeers.count)")
+        
+    }
+    
+    func uniq<S : SequenceType, T : Hashable where S.Generator.Element == T>(source: S) -> [T] {
+        var buffer = [T]()
+        var added = Set<T>()
+        for elem in source {
+            if !added.contains(elem) {
+                buffer.append(elem)
+                added.insert(elem)
+            }
+        }
+        return buffer
+    }
+//    
+//    let vals = [1, 4, 2, 2, 6, 24, 15, 2, 60, 15, 6]
+//    let uniqueVals = uniq(vals) // [1, 4, 2, 6, 24, 15, 60]
+//    
+    
+    
+    
+    
 }
